babb6f05ec7cdb4b932043edb508858a302279cc
stephen <stevedoyin@gmail.com>
2020-08-14 14:11:28 +0100

    dashboard empty

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,9 +1,19 @@
 let transactionSlider = $(".transaction-slider");
 
 transactionSlider.owlCarousel({
-  loop: false,
+  loop: true,
   margin: 10,
   nav: false,
-  dots: false,
+  dots: true,
   items: 1,
+
+  // nav: false,
+});
+
+// Custom Navigation Events
+$(".js-right-control").click(function () {
+  transactionSlider.trigger("next.owl.carousel");
+});
+$(".js-left-control").click(function () {
+  transactionSlider.trigger("prev.owl.carousel");
 });
